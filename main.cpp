2030f684616114d59e8d038807adbc44bfd909d8
Brandon Buttlar <bjbzrc@mail.missouri.edu>
2019-01-22 01:45:58 -0600

    Finished building and running code with no issues

--- main.cpp
+++ main.cpp
@@ -1,11 +1,37 @@
-// FizzBuzz project for IT 2040 SP '19
+// FizzBuzz project for INFOTC 2040 SP '19, done in 
+// Geany on Linux Mint 19.1
 // By Brandon Buttlar
 #include <iostream>
 using namespace std;
 
 int main()
 {
+	for (int i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			cout << "FizzBuzz\n";
+			continue;
+		}
+		
+		else if (i % 3 == 0)
+		{
+			cout << "Fizz\n";
+			continue;
+		}
+		
+		else if (i % 5 == 0)
+		{
+			cout << "Buzz\n";
+			continue;
+		}
+		
+		else
+		{
+			cout << i << endl;
+			continue;
+		}
+	}
 	
 	return 0;
 }
-
